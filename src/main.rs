227cc83bf7d66bcb0cb4ddd549e2b5b169e22c19
M3t0r <github@m3t0r.de>
2024-01-27 01:07:10 +0100

    main: Add clap and first CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 use slog::{Drain, o};
+use clap::{Parser, ValueEnum};
 
 pub mod smartctl;
 pub mod server;
 pub mod collector;
 
 use crate::smartctl::SmartctlInvoker;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum InvokerChoice {
+    /// call smartctl directly
+    Standard,
+    /// call smartctl via sudo so smart_exporter doesn't have to run as root
+    Sudo,
+    /// use smartctl output saved to files insteadd of calling smartctl directly
+    File,
+}
+
+/// Export S.M.A.R.T. drive stats in Prometheus format
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CLIOptions {
+    /// how to invoke smartctl
+    #[arg(short, long, default_value = "sudo")]
+    invoker: InvokerChoice,
+
+    /// in which path to look for files for the file invoker
+    #[arg(long, default_value = "./")]
+    file_invoker_path: PathBuf,
+
+    #[arg(short = 'b', long = "bind", default_value = "0.0.0.0:9940")]
+    bind_address: std::net::SocketAddr,
+}
 
 fn make_logger() -> slog::Logger {
     let decorator = slog_term::TermDecorator::new().build();
@@ -18,9 +45,11 @@ fn make_logger() -> slog::Logger {
 
 #[tokio::main]
 async fn main() {
+    let args = CLIOptions::parse();
+
     let log = make_logger();
-    let mut invoker = smartctl::FileInvoker::new(Path::new("./tests/simple/"));
-    //let mut invoker = smartctl::SudoInvoker{};
+    //let mut invoker = smartctl::FileInvoker::new(PathBuf::new("./tests/simple/"));
+    let mut invoker = smartctl::SudoInvoker{};
 
     // run once early on to check:
     //  - sudo available and configured
@@ -36,10 +65,21 @@ async fn main() {
 
     let server = server::Server::new(
         collector_channel,
-        "0.0.0.0:9940".parse().unwrap(),
+        args.bind_address,
         &log
     ).expect("failed to configure server");
 
     server.run().await.expect("failed to start server");
     collector_worker.await.expect("collector worker error");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::CLIOptions;
+    use clap::CommandFactory;
+
+    #[test]
+    fn verify_cli() {
+        CLIOptions::command().debug_assert();
+    }
 }
